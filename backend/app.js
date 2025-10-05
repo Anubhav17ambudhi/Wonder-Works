23a6530d34934fd227060877031d3627b63a9968
anubhavvishwakarma17@gmail.com <anubhavvishwakarma17@gmail.com>
2025-10-06 01:01:16 +0530

    login, logout, register controller

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,28 @@
 import express from "express";
 import { config } from "dotenv";
-
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 export const app = express();
+import { dbConnection } from "./db.js";
+import userRouter from "./routes/user.route.js";
 
 config({ path: "./.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,21 +1,67 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    role: {type: String, enum: ['citizen', 'admin', 'worker'], default: 'citizen'},
-    address: {type: String},
-    phone: {
-    type: String,
-        validate: {
-            validator: function(v) {
-                return /^\+91\d{10}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        unique: true,
-        required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    role: {
+      type: String,
+      enum: ["citizen", "admin", "worker"],
+      default: "citizen",
     },
-    myComplaints: [{type: mongoose.Schema.Types.ObjectId, ref: 'Complaint'}]
-}, {timestamps: true});
+    address: { type: String, required: false },
+    phone: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^\+91\d{10}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+      unique: true,
+      required: true,
+    },
+    myComplaints: [{ type: mongoose.Schema.Types.ObjectId, ref: "Complaint" }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  // If password is not modified, use 'return' to exit the function immediately
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // If password IS modified, hash it and then proceed
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000; // Token valid for 15 minutes
+  return resetToken;
+};
+
+export const User = mongoose.model("User", userSchema);
