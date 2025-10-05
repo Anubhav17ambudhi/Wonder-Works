742a4703908e1b794bed3a23f1d55e232f38221e
thefearlesscoder <thefunnelvkd@gmail.com>
2025-10-05 23:18:39 +0530

    create complaint schema

---
+++ backend/models/complaint.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const complaintSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: { type: String, required: true },
+
+    description: { type: String, required: true },
+
+    status: {
+      type: String,
+      enum: ["open", "in_progress", "resolved", "closed"],
+      default: "open",
+    },
+
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+
+    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    area: { type: String, required: true },
+
+    location_details: { type: String, required: true },
+
+    type: {
+      type: String,
+      enum: ["infrastructure", "service", "other"],
+      default: "other",
+    },
+
+    photo_url: { type: String },
+
+    updated_by_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+// create a model
+const Complaint = mongoose.model("Complaint", complaintSchema);
+export default Complaint;
